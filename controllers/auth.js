69afeebb2933e6798a69cf67f5ed8d01503c7a36
Ashish <ashishskabade@gmail.com>
2023-03-26 19:17:57 +0530

    new version

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const config = process.env;
+const { config } = require("../config/config");
 
 const verifyToken = (req, res, next) => {
   const token = req.headers["x-access-token"];
@@ -9,12 +9,12 @@ const verifyToken = (req, res, next) => {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, "xube8c93e9ebx8bec99--r2g7---ce8bw8xb8becyrvcbehbuebx");
-    console.log(decoded)
+    const decoded = jwt.verify(token, config.app.app_key);
+    console.log(decoded);
     req.body.userId = decoded.userId;
     req.query.userId = decoded.userId;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).json({ error: "Invalid Token" });
   }
   return next();
 };
